86cbb94c4bb3e7f8ab51e746c1fbb42adf86ff50
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2023-01-11 10:52:01 -0500

    import csv files as data frames

--- bikeshare_wrangling.R
+++ bikeshare_wrangling.R
@@ -2,5 +2,19 @@
 library(tidyverse)
 library(lubridate) # wrangle dates, time
 
-# import files
+# change wd to access csv files
+setwd("/Users/laurafontanills/Documents/datasets/divvy_csv_monthly")
 
+# import csv files as data frames
+rides_202201 <- read.csv("202201-divvy-tripdata.csv") # January 2022
+rides_202202 <- read.csv("202202-divvy-tripdata.csv") #February 2022
+rides_202203 <- read.csv("202203-divvy-tripdata.csv") # March 2022
+rides_202204 <- read.csv("202204-divvy-tripdata.csv") # April 2022
+rides_202205 <- read.csv("202205-divvy-tripdata.csv") # May 2022
+rides_202206 <- read.csv("202206-divvy-tripdata.csv") # June 2022
+rides_202207 <- read.csv("202207-divvy-tripdata.csv") # July 2022
+rides_202208 <- read.csv("202208-divvy-tripdata.csv") # August 2022
+rides_202209 <- read.csv("202209-divvy-tripdata.csv") # September 2022
+rides_202210 <- read.csv("202210-divvy-tripdata.csv") # October 2022
+rides_202211 <- read.csv("202211-divvy-tripdata.csv") # November 2022
+rides_202212 <- read.csv("202212-divvy-tripdata.csv") # December 2022
