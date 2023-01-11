9174931f8d2596d7d88a6a81580f7ee7f75e1382
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2023-01-11 17:16:12 -0500

    added bike type analysis

--- bikeshare_analysis.R
+++ bikeshare_analysis.R
@@ -49,3 +49,32 @@ rides_all %>%
   arrange(member_casual, month) %>% 
   ggplot(aes(x=month, y=average_duration, fill=member_casual)) +
   geom_col(position = "dodge")
+
+
+# compare bike types, ride length
+rides_all %>% 
+  group_by(member_casual, rideable_type) %>% 
+  summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
+  arrange(member_casual, rideable_type) %>% 
+  ggplot(aes(x=rideable_type, y=average_duration, fill=member_casual)) +
+  geom_col(position = "dodge")
+
+# compare bike types, number of rides
+rides_all %>% 
+  group_by(member_casual, rideable_type) %>% 
+  summarize(number_of_rides = n()) %>% 
+  arrange(member_casual, rideable_type) %>% 
+  ggplot(aes(x=rideable_type, y=number_of_rides, fill=member_casual)) +
+  geom_col(position = "dodge")
+
+# compare bike type, ride length, weekday
+rides_all %>% 
+  group_by(member_casual, rideable_type, weekday) %>% 
+  summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
+  arrange(member_casual, rideable_type)
+
+# compare bike type, number of rides, month of year
+  rides_all %>% 
+  group_by(member_casual, rideable_type, month) %>% 
+  summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
+  arrange(member_casual, rideable_type)
