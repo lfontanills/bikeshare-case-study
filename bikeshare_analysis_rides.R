0d831ff58b4e45ad563f0150c1e22691d6d32333
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2023-01-12 10:33:55 -0500

    Fixed variable names

--- bikeshare_analysis_rides.R
+++ bikeshare_analysis_rides.R
@@ -11,44 +11,44 @@ aggregate(rides_all$ride_length ~ rides_all$member_casual, FUN = max)
 aggregate(rides_all$ride_length ~ rides_all$member_casual, FUN = min)
 
 # compare average ride time per day of week
-aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$weekday, FUN = mean)
+aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$ride_weekday, FUN = mean)
 
 # compare average ride time by day of month
-aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$day, FUN = mean)
+aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$ride_day, FUN = mean)
 
 # compare average ride time by month
-aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$month, FUN = mean)
+aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$ride_month, FUN = mean)
 
 # compare ridership by weekday, create visualization
 rides_all %>% 
-  group_by(member_casual, weekday) %>% 
+  group_by(member_casual, ride_weekday) %>% 
   summarize(number_of_rides = n(), average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, weekday) %>% 
-  ggplot(aes(x=weekday, y=number_of_rides, fill=member_casual)) +
+  arrange(member_casual, ride_weekday) %>% 
+  ggplot(aes(x=ride_weekday, y=number_of_rides, fill=member_casual)) +
   geom_col(position = "dodge")
 
 # compare ridership by month, create visualization
 rides_all %>% 
-  group_by(member_casual, month) %>% 
+  group_by(member_casual, ride_month) %>% 
   summarize(number_of_rides = n(), average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, month) %>% 
-  ggplot(aes(x=month, y=number_of_rides, fill=member_casual)) +
+  arrange(member_casual, ride_month) %>% 
+  ggplot(aes(x=ride_month, y=number_of_rides, fill=member_casual)) +
   geom_col(position = "dodge")
 
 # compare ride length by weekday, create visualization
 rides_all %>% 
-  group_by(member_casual, weekday) %>% 
+  group_by(member_casual, ride_weekday) %>% 
   summarize(number_of_rides = n(), average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, weekday) %>% 
-  ggplot(aes(x=weekday, y=average_duration, fill=member_casual)) +
+  arrange(member_casual, ride_weekday) %>% 
+  ggplot(aes(x=ride_weekday, y=average_duration, fill=member_casual)) +
   geom_col(position = "dodge")
 
 # compare ride length by month, create visualization
 rides_all %>% 
-  group_by(member_casual, month) %>% 
+  group_by(member_casual, ride_month) %>% 
   summarize(number_of_rides = n(), average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, month) %>% 
-  ggplot(aes(x=month, y=average_duration, fill=member_casual)) +
+  arrange(member_casual, ride_month) %>% 
+  ggplot(aes(x=ride_month, y=average_duration, fill=member_casual)) +
   geom_col(position = "dodge")
 
 
@@ -70,19 +70,19 @@ rides_all %>%
 
 # compare bike type, ride length, weekday
 rides_all %>% 
-  group_by(member_casual, rideable_type, weekday) %>% 
+  group_by(member_casual, rideable_type, ride_weekday) %>% 
   summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
   arrange(member_casual, rideable_type)
 
 # compare bike type, number of rides, month of year
   rides_all %>% 
-  group_by(member_casual, rideable_type, month) %>% 
+  group_by(member_casual, rideable_type, ride_month) %>% 
   summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
   arrange(member_casual, rideable_type)
   
 # create summary data frames
-avg_ride_length <- aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$weekday + rides_all$month + rides_all$rideable_type, FUN = mean)
-median_ride_length <- aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$weekday + rides_all$month + rides_all$rideable_type, FUN = median)
+avg_ride_length <- aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$ride_weekday + rides_all$ride_month + rides_all$rideable_type, FUN = mean)
+median_ride_length <- aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$ride_weekday + rides_all$ride_month + rides_all$rideable_type, FUN = median)
 
 # export as csv
 write.csv(avg_ride_length, file = '~/Documents/datasets/avg_ride_length.csv')
