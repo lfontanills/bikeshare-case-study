b579d09ee4d9f7ff9f429548105ebf4ad696f936
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2023-01-12 17:18:55 -0500

    Cleaned up code - data summaries

--- bikeshare_analysis_rides.R
+++ bikeshare_analysis_rides.R
@@ -37,60 +37,79 @@ aggregate(rides_all$ride_length ~ rides_all$member_casual, FUN = min)
 
 # Summarize ride duration each month (quick visualization)
 rides_all %>% 
-  group_by(ride_month, member_casual) %>% 
+  group_by(member_casual, ride_month) %>% 
   summarize(median_ride_length = median(ride_length)) %>% 
   ggplot(aes(x=ride_month, y=median_ride_length, fill=member_casual)) +
   geom_col(position = "dodge")
 
 # Summarize ride duration each weekday (quick visualization)
 rides_all %>% 
-  group_by(ride_weekday, member_casual) %>% 
+  group_by(member_casual, ride_weekday) %>% 
   summarize(median_ride_length = median(ride_length)) %>% 
   ggplot(aes(x=ride_weekday, y=median_ride_length, fill=member_casual)) +
   geom_col(position = "dodge")
 
 # Summarize ride duration each hour (quick visualization)
 rides_all %>% 
-  group_by(ride_hour, member_casual) %>% 
+  group_by(member_casual, ride_hour) %>% 
   summarize(median_ride_length = median(ride_length)) %>% 
   ggplot(aes(x=ride_hour, y=median_ride_length, fill=member_casual)) +
   geom_col(position = "dodge")
 
 
+# Descriptive analysis of BIKE TYPES by user type (member_casual)
 
-
-# compare bike types, ride length, create quick visualization
+# Summarize number of rides per bike type by user type over the entire year
+# Note - small % of rides are docked_bike, all casual users
 rides_all %>% 
   group_by(member_casual, rideable_type) %>% 
-  summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, rideable_type) %>% 
-  ggplot(aes(x=rideable_type, y=average_duration, fill=member_casual)) +
+  summarize(total_count=n()) %>% 
+  ggplot(aes(x=rideable_type, y=total_count, fill=member_casual)) +
   geom_col(position = "dodge")
 
-# compare bike types, number of rides,create quick visualization
+# Summarize ride duration per bike type by user type over the entire year
+# Note - docked_bike rides account for very long ride durations
 rides_all %>% 
   group_by(member_casual, rideable_type) %>% 
-  summarize(number_of_rides = n()) %>% 
-  arrange(member_casual, rideable_type) %>% 
-  ggplot(aes(x=rideable_type, y=number_of_rides, fill=member_casual)) +
+  summarize(median_ride_length = median(ride_length)) %>% 
+  ggplot(aes(x=rideable_type, y=median_ride_length, fill=member_casual)) +
   geom_col(position = "dodge")
 
-# compare bike type, ride length, weekday
+# Summarize number of rides per bike type by user type over each month
+# Note - filtering out docked_bike
 rides_all %>% 
-  group_by(member_casual, rideable_type, ride_weekday) %>% 
-  summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, rideable_type)
-
-# compare bike type, number of rides, month of year
-  rides_all %>% 
+  filter(rideable_type != "docked_bike") %>% 
   group_by(member_casual, rideable_type, ride_month) %>% 
-  summarize(sum(number_of_rides = n()), average_duration = mean(ride_length)) %>% 
-  arrange(member_casual, rideable_type)
+  summarize(total_count=n(), member_casual) %>% 
+  arrange(member_casual, rideable_type) %>% 
+  ggplot(aes(x=ride_month, y=total_count, fill=rideable_type)) +
+  facet_wrap(~member_casual) +
+  geom_col(position = "dodge")
+
+# Summarize number of rides per bike type by user type over each weeday
+# Note - filtering out docked_bike
+rides_all %>% 
+  filter(rideable_type != "docked_bike") %>% 
+  group_by(member_casual, rideable_type, ride_weekday) %>% 
+  summarize(total_count=n(), member_casual) %>% 
+  arrange(member_casual, rideable_type) %>% 
+  ggplot(aes(x=ride_weekday, y=total_count, fill=rideable_type)) +
+  facet_wrap(~member_casual) +
+  geom_col(position = "dodge")
+
+
+
   
 # create summary data frames
 avg_ride_length <- aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$ride_weekday + rides_all$ride_month + rides_all$rideable_type, FUN = mean)
 median_ride_length <- aggregate(rides_all$ride_length ~ rides_all$member_casual + rides_all$ride_weekday + rides_all$ride_month + rides_all$rideable_type, FUN = median)
+# create summary files
+bike_types <- rides_all %>% 
+  group_by(member_casual, rideable_type, ride_weekday, ride_month) %>% 
+  summarize(number_of_rides = n(), average_duration = mean(ride_length)) %>% 
+  arrange(member_casual, rideable_type)
 
 # export as csv
 write.csv(avg_ride_length, file = '~/Documents/datasets/avg_ride_length.csv')
 write.csv(median_ride_length, file = '~/Documents/datasets/median_ride_length.csv')
+write.csv(bike_types, file = '~/Documents/datasets/bike_types.csv')
