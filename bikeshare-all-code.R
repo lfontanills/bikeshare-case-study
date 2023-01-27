a43839fed3b5bc33bc60761e425af68dc85187d7
Laura Fontanills <109815517+lfontanills@users.noreply.github.com>
2023-01-27 18:21:43 -0500

    Edited and proofread

--- bikeshare-all-code.R
+++ bikeshare-all-code.R
@@ -226,3 +226,174 @@ rides_clean <-rides_2022 %>%
 
 # export clean df as csv for analysis
 write.csv(rides_clean, "~/Documents/projects/bikeshare-case-study/rides_clean.csv") 
+
+
+#########
+
+
+## Analysis + Viz
+
+####
+library(tidyverse)
+library(lubridate)
+
+
+## Ride durations
+
+rides_clean %>% 
+  group_by(member_casual) %>% 
+  summarize(num_rides=n()) %>% 
+  ggplot(aes(x=member_casual, y=num_rides, fill=member_casual)) +
+  geom_col(position="dodge")
+
+rides_clean %>% 
+  group_by(member_casual) %>% 
+  summary(rides_clean$ride_length)
+
+tapply(rides_clean$ride_length, rides_clean$member_casual, summary)
+
+# check if normal distribution
+
+hist(subset(rides_clean, member_casual == "member")$ride_length,
+     main = "Ride lengths for members",
+     xlab = "Ride length (seconds)")
+
+hist(subset(rides_clean, member_casual == "casual")$ride_length,
+     main = "Ride lengths for casual users",
+     xlab = "Ride length (seconds)")
+
+users_length <- subset(rides_clean, member_casual = 'member', select=c('member_casual', 'ride_length'))
+
+wilcox.test(users_length$ride_length ~ users_length$member_casual)
+
+# graph ride durations
+
+length_plot <-
+  rides_clean %>% 
+  group_by(member_casual) %>% 
+  summarize(number_of_rides = n(), mean_length = mean(ride_length)) %>% 
+  arrange(member_casual) %>% 
+  ggplot(aes(x=member_casual, y=mean_length, fill=member_casual)) +
+  geom_col(position = "dodge") +
+  labs(
+    title = "Average ride duration",
+    subtitle = "For all trips between 1 minute and 3 hours long",
+    x = "User type", 
+    y = "Average ride duration (seconds)",
+    fill = "User type") +
+  scale_x_discrete(labels = c("classic bike", "electric bike")) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw() + 
+  scale_fill_brewer(palette = "Greens", direction = -1)
+
+## Ride times
+
+# ride counts by time period
+
+rides_clean %>% 
+  group_by(member_casual) %>% 
+  summarize(count=n()) %>% 
+  ggplot(aes(x=member_casual, y=count, fill=member_casual)) +
+  geom_col(position="dodge")
+
+rides_clean %>% 
+  mutate(month = month(started_at, label = TRUE, abbr = FALSE)) %>% 
+  filter(month == "June" | month == "July" |month == "August") %>% 
+  group_by(member_casual) %>% 
+  summarize(
+    count = n(),
+    median = median(ride_length, na.rm = TRUE),
+    IQR = IQR(ride_length, na.rm = TRUE)
+  ) 
+
+# by month of year
+rides_clean %>% 
+  mutate(month = month(started_at, label = TRUE, abbr = FALSE)) %>% 
+  group_by(member_casual, month) %>% 
+  summarize(count=n()) %>% 
+  ggplot(aes(x=month, y=count, fill=member_casual))+
+  geom_col(position="dodge")
+
+# by day of week
+weekday_plot <-
+  rides_clean %>% 
+  mutate(weekday = wday(started_at, label = TRUE, abbr = FALSE)) %>% 
+  group_by(member_casual, weekday) %>% 
+  summarize(count=n()) %>% 
+  ggplot(aes(x=weekday, y=count, fill=member_casual))+
+  geom_col(position="dodge") +
+  labs(
+    title = "Number of rides by weekday",
+    subtitle = "For all trips between 1 minute and 24 hours long",
+    x = "User type", 
+    y = "Number of rides",
+    fill = "User type") +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw() + 
+  scale_fill_brewer(palette = "Greens", direction = -1)
+
+# by hour of day
+
+rides_clean %>% 
+  mutate(hour = hour(started_at)) %>% 
+  group_by(member_casual, hour) %>% 
+  summarize(count=n()) %>% 
+  ggplot(aes(x=hour, y=count, fill=member_casual))+
+  geom_col(position="dodge")
+
+
+## Ride locations
+
+casual_stations<-
+  rides_clean %>% 
+  filter(member_casual == "casual") %>% 
+  group_by(member_casual, start_station_name) %>% 
+  summarize(num_rides = n()) %>% 
+  arrange(desc(num_rides)) %>% 
+  mutate(percent_rides = 100*(num_rides/sum(num_rides))) %>% 
+  head(10)
+
+member_stations <-
+  rides_clean %>% 
+  filter(member_casual == "member") %>% 
+  group_by(member_casual, start_station_name) %>% 
+  summarize(num_rides = n()) %>% 
+  arrange(desc(num_rides)) %>% 
+  mutate(percent_rides = 100*(num_rides/sum(num_rides))) %>% 
+  head(10)
+
+top_stations <- rbind(casual_stations, member_stations)
+
+locations <-rides_all_clean[c(5, 9:10)]
+
+# create csvs -export for mapping
+write_csv(top_stations, "~/Desktop/top_stations.csv")
+write_csv(locations, "~/Desktop/locations.csv")
+
+## Bike types
+
+rides_clean %>% 
+  group_by(member_casual, rideable_type) %>%
+  summarize(num_rides = n()) %>% 
+  mutate(pct_rides = 100* num_rides/sum(num_rides))
+
+
+bike_type_plot <- rides_clean %>% 
+  group_by(rideable_type, member_casual) %>% 
+  filter(rideable_type == "classic_bike" | rideable_type == "electric_bike") %>% 
+  summarize(num_rides = n()) %>% 
+  arrange(member_casual) %>% 
+  ggplot(aes(x=member_casual, y=num_rides, fill=rideable_type)) +
+  geom_col(position = "dodge") +
+  labs(
+    title = "Number of rides with Classic Bikes, Electric Bikes",
+    subtitle = "For all trips between 1 minute and 3 hours long",
+    x = "Membership type", 
+    y = "Number of rides",
+    fill = "Bike type", 
+    labels = c("classic_bike", "electric_bike")) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw() + 
+  scale_fill_brewer(palette = "Blues")
+
+bike_type_plot
--- bikeshare_analysis_update.R
+++ bikeshare_analysis_update.R
@@ -1,15 +1,69 @@
-#### 
-# What is the difference in ride frequency for members and casual riders?
+####
+library(tidyverse)
+library(lubridate)
+
+
+## Ride durations
+
+rides_clean %>% 
+  group_by(member_casual) %>% 
+  summarize(num_rides=n()) %>% 
+  ggplot(aes(x=member_casual, y=num_rides, fill=member_casual)) +
+  geom_col(position="dodge")
+
+rides_clean %>% 
+  group_by(member_casual) %>% 
+  summary(rides_clean$ride_length)
+
+tapply(rides_clean$ride_length, rides_clean$member_casual, summary)
+
+# check if normal distribution
+
+
+ggplot(rides_clean, aes(x = ride_length)) +
+  geom_histogram() +
+  facet_wrap(~ member_casual) +
+  labs(
+    x = "ride length", 
+    y = "number of rides") +
+  scale_y_continuous(labels = scales::comma)
+
+
+users_length <- subset(rides_clean, member_casual = 'member', select=c('member_casual', 'ride_length'))
+
+wilcox.test(users_length$ride_length ~ users_length$member_casual)
+
+# graph ride durations
+
+length_plot <-
+  rides_clean %>% 
+  group_by(member_casual) %>% 
+  summarize(number_of_rides = n(), mean_length = mean(ride_length)) %>% 
+  arrange(member_casual) %>% 
+  ggplot(aes(x=member_casual, y=mean_length, fill=member_casual)) +
+  geom_col(position = "dodge") +
+  labs(
+    title = "Average ride duration",
+    subtitle = "For all trips between 1 minute and 3 hours long",
+    x = "User type", 
+    y = "Average ride duration (seconds)",
+    fill = "User type") +
+  scale_x_discrete(labels = c("classic bike", "electric bike")) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw() + 
+  scale_fill_brewer(palette = "Greens", direction = -1)
+
+## Ride times
 
 # ride counts by time period
 
-rides_all_clean %>% 
+rides_clean %>% 
   group_by(member_casual) %>% 
   summarize(count=n()) %>% 
   ggplot(aes(x=member_casual, y=count, fill=member_casual)) +
   geom_col(position="dodge")
 
-rides_all_clean %>% 
+rides_clean %>% 
   mutate(month = month(started_at, label = TRUE, abbr = FALSE)) %>% 
   filter(month == "June" | month == "July" |month == "August") %>% 
   group_by(member_casual) %>% 
@@ -18,17 +72,18 @@ rides_all_clean %>%
     median = median(ride_length, na.rm = TRUE),
     IQR = IQR(ride_length, na.rm = TRUE)
   ) 
-  
-rides_all_clean %>% 
+
+# by month of year
+rides_clean %>% 
   mutate(month = month(started_at, label = TRUE, abbr = FALSE)) %>% 
   group_by(member_casual, month) %>% 
   summarize(count=n()) %>% 
   ggplot(aes(x=month, y=count, fill=member_casual))+
   geom_col(position="dodge")
 
-# Number of rides by time of week
+# by day of week
 weekday_plot <-
-  rides_all_clean %>% 
+  rides_clean %>% 
   mutate(weekday = wday(started_at, label = TRUE, abbr = FALSE)) %>% 
   group_by(member_casual, weekday) %>% 
   summarize(count=n()) %>% 
@@ -44,7 +99,9 @@ weekday_plot <-
   theme_bw() + 
   scale_fill_brewer(palette = "Greens", direction = -1)
 
-rides_all_clean %>% 
+# by hour of day
+
+rides_clean %>% 
   mutate(hour = hour(started_at)) %>% 
   group_by(member_casual, hour) %>% 
   summarize(count=n()) %>% 
@@ -52,76 +109,24 @@ rides_all_clean %>%
   geom_col(position="dodge")
 
 
-####
-# What is the difference in ride duration for members and casual riders?
+## Ride locations
 
-# summary statistics
-rides_all_clean %>% 
-  group_by(member_casual) %>% 
-  summarize(min = min(ride_length),
-            q1 = quantile(ride_length, 0.25),
-            median = median(ride_length),
-            mean = mean(ride_length),
-            q3 = quantile(ride_length, 0.75),
-            max = max(ride_length),
-            sd = sd(ride_length)
-            )
-
-# check if normal distribution - NOPE
-member_length_hist <-
-hist(subset(rides_all_clean, member_casual == "member")$ride_length,
-     main = "Ride lengths for members",
-     xlab = "Ride length")
-
-casual_length_hist <-
-hist(subset(rides_all_clean, member_casual == "casual")$ride_length,
-     main = "Ride lengths for casual users",
-     xlab = "Ride length")
-
-# statistical test
-users_length <- subset(rides_all_clean, member_casual = 'member', select=c('member_casual', 'ride_length'))
-
-wilcox.test(users_length$ride_length ~ users_length$member_casual) # reject null hypothesis
-
-# visualize difference in ride duration
-
-# Average duration
-length_plot <-
-rides_all_clean %>% 
-  group_by(member_casual) %>% 
-  summarize(number_of_rides = n(), mean_length = mean(ride_length)) %>% 
-  arrange(member_casual) %>% 
-  ggplot(aes(x=member_casual, y=mean_length, fill=member_casual)) +
-  geom_col(position = "dodge") +
-  labs(
-    title = "Average ride duration",
-    subtitle = "For all trips between 1 minute and 24 hours long",
-    x = "User type", 
-    y = "Average ride duration (seconds)",
-    fill = "User type") +
-  scale_x_discrete(labels = c("classic bike", "electric bike")) +
-  scale_y_continuous(labels = scales::comma) +
-  theme_bw() + 
-  scale_fill_brewer(palette = "Greens", direction = -1)
-
-####
-# What is the difference in ride locations for members and casual users
 casual_stations<-
-rides_all_clean %>% 
+  rides_clean %>% 
   filter(member_casual == "casual") %>% 
   group_by(member_casual, start_station_name) %>% 
-  summarize(count = n()) %>% 
-  arrange(desc(count)) %>% 
-  mutate(percent = 100*(count/sum(count))) %>% 
+  summarize(num_rides = n()) %>% 
+  arrange(desc(num_rides)) %>% 
+  mutate(percent_rides = 100*(num_rides/sum(num_rides))) %>% 
   head(10)
 
 member_stations <-
-rides_all_clean %>% 
+  rides_clean %>% 
   filter(member_casual == "member") %>% 
   group_by(member_casual, start_station_name) %>% 
-  summarize(count = n()) %>% 
-  arrange(desc(count)) %>% 
-  mutate(percent = 100*(count/sum(count))) %>% 
+  summarize(num_rides = n()) %>% 
+  arrange(desc(num_rides)) %>% 
+  mutate(percent_rides = 100*(num_rides/sum(num_rides))) %>% 
   head(10)
 
 top_stations <- rbind(casual_stations, member_stations)
@@ -132,4 +137,32 @@ locations <-rides_all_clean[c(5, 9:10)]
 write_csv(top_stations, "~/Desktop/top_stations.csv")
 write_csv(locations, "~/Desktop/locations.csv")
 
-            
+## Bike types
+
+rides_clean %>% 
+  group_by(member_casual, rideable_type) %>%
+  summarize(num_rides = n()) %>% 
+  mutate(pct_rides = 100* num_rides/sum(num_rides))
+  
+
+bike_type_plot <- rides_clean %>% 
+  group_by(rideable_type, member_casual) %>% 
+  filter(rideable_type == "classic_bike" | rideable_type == "electric_bike") %>% 
+  summarize(num_rides = n()) %>% 
+  arrange(member_casual) %>% 
+  ggplot(aes(x=member_casual, y=num_rides, fill=rideable_type)) +
+  geom_col(position = "dodge") +
+  labs(
+    title = "Number of rides with Classic Bikes, Electric Bikes",
+    subtitle = "For all trips between 1 minute and 3 hours long",
+    x = "Membership type", 
+    y = "Number of rides",
+    fill = "Bike type", 
+    labels = c("classic_bike", "electric_bike")) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw() + 
+  scale_fill_brewer(palette = "Blues")
+
+bike_type_plot
+
+
